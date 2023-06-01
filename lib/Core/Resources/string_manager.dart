5ef6b4494e86cd692056ba982260ea1d0e4fd657
manarshahin <o-manar.shahin@etisalat.com>
2023-06-01 19:55:14 +0300

    third screen done

--- lib/Core/Resources/string_manager.dart
+++ lib/Core/Resources/string_manager.dart
@@ -1,8 +1,19 @@
 class StringManager {
   static const String noRouteFound = "noRouteFound";
   static const String title = "Reading is Window to the Future";
-  static const String subTitle = "Reading Can Add Broad Knowledge And Make Smart";
+  static const String subTitle =
+      "Reading Can Add Broad Knowledge And Make Smart";
   static const String btnText = "Let's Read";
   static const String welcome = "Welcome Back, Adam!";
   static const String secondViewTitle = "Read News Increase Your Knowledge";
+  static const String detailNew = "Detail News";
+  static const String date = "28 Januari 2020 | 08.53";
+  static const String description = '''Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et
+acusam et justo duo dolores et ea rebum.
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua.
+  ''';
 }
--- lib/Presentation/third_view.dart
+++ lib/Presentation/third_view.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import '../Core/Resources/uilites.dart';
 
 class ThirdView extends StatelessWidget {
   const ThirdView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: InkWell(
+            child: const Icon(
+              Icons.arrow_back,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(
+            StringManager.detailNew,
+            style: getSemiBoldStyle(
+              color: ColorManager.white,
+              fontSize: 20,
+            ),
+          ),
+          actions: [
+            InkWell(
+              child: const Icon(
+                Icons.share,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Image.asset(
+              ImageManager.mainImg,
+              height: 300,
+              width: 400,
+              fit: BoxFit.fitWidth,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    StringManager.secondViewTitle,
+                    style: getBoldStyle(
+                        color: ColorManager.secondary, fontSize: 28),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    StringManager.date,
+                    style: getLightStyle(
+                        color: ColorManager.secondaryLight, fontSize: 12),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    StringManager.description,
+                    style: getLightStyle(
+                        color: ColorManager.secondaryLight, fontSize: 15),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
